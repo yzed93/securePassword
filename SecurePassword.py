8afa25d9694944d1851b8935483f0a4ec7e3f097
yzed93 <dennis.wiredu@yahoo.de>
2022-02-26 16:58:25 +0100

    Commit Test Visual Studio

--- SecurePassword.py
+++ SecurePassword.py
@@ -4,4 +4,6 @@ import string
 chars = string.digits + string.ascii_lowercase + string.ascii_uppercase+string.punctuation
 print("".join(secrets.choice(chars) for _ in range(16)))
 
+## git commit test ##
+
 
